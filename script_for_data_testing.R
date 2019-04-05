21a5d15562d3d77c4962424dcf8e9f77f788f9fe
Beau Meche <beau_meche@college.harvard.edu>
2019-04-05 01:16:00 -0400

    initial commit with the relevant data

---
+++ script_for_data_testing.R
@@ -0,0 +1,8 @@
+
+library(tidyverse)
+library(readr)
+
+
+census_2017 <- read_csv(file = "Census_2017_edu_attainment.csv")
+census_2016 <- read_csv(file = "Census_edu_attainment_2016.csv")
+census_2015 <- read_csv(file = "Census_edu_attainment_2015.csv")
